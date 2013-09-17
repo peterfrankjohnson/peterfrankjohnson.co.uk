2b97aec436d8404bd455b6cfd169e9e8a74957c5
Peter Johnson <peter.johnson@peterfrankjohnson.co.uk>
2013-09-17 02:36:34 +0100

    Stripped unnecessary code.

--- Database.php
+++ Database.php
@@ -4,56 +4,112 @@ class Database extends SQLite3
 {
     function __construct()
     {
-        $this->open('mysqlitedb.db');
+        $this->open('sqlite3.db');
+    }
+}
+
+class Request {
+    public static function Method(){
+        if(isset($_SERVER['REQUEST_METHOD'])) {
+            return $_SERVER['REQUEST_METHOD'];
+        }
+        return null;
+    }
+
+    public static function Uri(){
+        if(isset($_SERVER['REQUEST_URI'])) {
+            return $_SERVER['REQUEST_URI'];
+        }
+        return null;
+    }
+
+    public static function QueryString(){
+        if(isset($_SERVER['QUERY_STRING'])) {
+            return $_SERVER['QUERY_STRING'];
+        }
+        return null;
+    }
+
+    public static function Protocol(){
+        if(isset($_SERVER['SERVER_PROTOCOL'])) {
+            return $_SERVER['SERVER_PROTOCOL'];
+        }
+        return null;
+    }
+
+    public static function Time(){
+        if(isset($_SERVER['REQUEST_TIME'])) {
+            return $_SERVER['REQUEST_TIME'];
+        }
+        return null;
+    }
+
+    public static function Header($key) {
+        $key = strtoupper(str_replace('-', '_', $key));
+        $key = 'HTTP_' . $key;
+        if(isset($_SERVER[$key])) {
+            return (string) "" . $_SERVER[$key];
+        }
+        return (string) "";
     }
 }
 
 $database = new Database();
+
 $database->exec(
     'CREATE TABLE IF NOT EXISTS request (
         id INTEGER PRIMARY KEY,
         method STRING,
-        uri STRING
+        uri STRING,
+        query STRING,
+        protocol STRING,
+        time INTEGER
     )'
 );
+
 $database->exec(
     'CREATE TABLE IF NOT EXISTS request_header (
         id INTEGER PRIMARY KEY,
         request_id INTEGER,
+        name STRING,
         header STRING
     )'
 );
 $database->exec(
     'INSERT INTO request VALUES(
         NULL, ' .
-        "'" . $_SERVER['REQUEST_METHOD'] . "', " .
-        "'" . $_SERVER['REQUEST_URI'] . "'" .
+        "'" . Request::Method() . "', " .
+        "'" . Request::Uri() . "', " .
+        "'" . Request::QueryString() . "', " .
+        "'" . Request::Protocol() . "', " .
+        "'" . Request::Time() . "'" .
     ')'
 );
 
 $requestId = $database->lastInsertRowId();
-$database->exec(
-    'INSERT INTO request_header VALUES( ' .
-        'NULL, ' .
-        $requestId . ', ' .
-        "'" . $_SERVER['HTTP_COOKIE'] . "'" .
-    ')'
+
+$headers = array(
+    'Host',
+    'User-agent',
+    'Cookie',
+    'Referer',
+    'Accept',
+    'Accept-Charset',
+    'Accept-Encoding',
+    'Accept-Language',
+    'Connection'
 );
 
-?>
-<table>
-<tr>
-<th>ID</th><th>Method</th><th>Uri</th>
-</tr>
-<?php
-$results = $database->query('SELECT * FROM request');
-while ($row = $results->fetchArray()) {
-?>
-<tr>
-<td><?= $row['id'] ?></td><td><?= $row['method'] ?></td><td><?= $row['uri']?></td>
-
-</tr>
-<?php
+foreach ($headers as $headerName) {
+    $headerValue = Request::Header($headerName);
+    if($headerValue != "") {
+        $database->exec(
+            'INSERT INTO request_header VALUES( ' .
+                'NULL, ' .
+                $requestId . ', ' .
+                "'" . $headerName . "', " .
+                "'" .  . "'" .
+            ')'
+        );
+    }
 }
-?>
-</table>
--- index.php
+++ index.php
@@ -8,33 +8,18 @@ define('SITEMAPS_ORG_XML', '/sitemap.xml');
 define('DOMAIN',           'peterfrankjohnson.co.uk');
 define('HOSTNAME',         'www.peterfrankjohnson.co.uk');
 
-// 1. Read all Request Data
-//  - Method
-//  - URI
-//  - 
-
-function getRequestUri() {
-    $requestUri = null;
-    if(isset($_SERVER['REQUEST_URI'])) {
-        $requestUri = $_SERVER['REQUEST_URI'];
-    }
-    return $requestUri;
-}
-
-function getRequestMethod() {
-
-}
+require_once('Database.php');
 
 function getCookie() {
     $cookie = null;
     if(isset($_SERVER['HTTP_COOKIE'])) {
         $cookie = $_SERVER['HTTP_COOKIE'];
     }
-return $cookie;
+    return $cookie;
 }
 
 function createSession() {
-
+    
 }
 
 function getSessionId() {
@@ -44,95 +29,110 @@ function getSessionId() {
 
 }
 
-$requestUri = getRequestUri();
+$requestUri = Request::Uri();
 
 switch($requestUri) {
     case ROBOTS_TXT: {
         $url = HOSTNAME . SITEMAPS_ORG_XML;
 
-        header('Content-Type: text/plain');
-        print <<<END
+  	header('Content-Type: text/plain');
+  	print <<<END
 Sitemap: http://$url
 
 User-agent: *
 Disallow:
 END;
         unset($url);
-	exit;
-    	break;
+  	break;
     }
+
     case SITEMAPS_ORG_XML: {
-        $urls = array (
+  	$urls = array (
             'http://' . HOSTNAME . '/',
             'http://' . HOSTNAME . '/test1',
             'http://' . HOSTNAME . '/test2',
             'http://' . HOSTNAME . '/test3'
         );
-print <<<END
+        header('Content-Type: text/xml');
+  	print <<<END
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 END;
+
         foreach($urls as $url) {
             print '  <loc>' . $url . '</loc>';
         }
 
-        print <<<END
+print <<<END
 </urlset>
 END;
-	exit;
         break;
     }
 
     case '/test1': {
-        print <<<END
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Test 1</title>
-    </head>
-    <body>
-        <p>Test 1</p>
-    </body>
-</html>
-END;
-	exit;
-        break;
+        echo 'TEST 1';
+  	break;
     }
 
     case '/test2': {
-        print <<<END
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Test 2</title>
-    </head>
-    <body>
-        <p>Test 2</p>
-    </body>
-</html>
-END;
-	exit;
-        break;
+        echo 'TEST 2';
+  	break;
     }
 
     case '/test3': {
-        print <<<END
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Test 3</title>
-    </head>
-    <body>
-        <p>Test 3</p>
-    </body>
-</html>
-END;
-	exit;
+        echo 'TEST 3';
+  	break;
+    }
+
+    case '/': {
+        $results = $database->query('SELECT * FROM request');
+        if($results instanceof SQLite3Result) {
+?>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Method</th>
+        <th>Uri</th>
+        <th>Query</th>
+        <th>Protocol</th>
+        <th>Time</th>
+        <th>Headers</th>
+    </tr>
+<?php       while ($row = $results->fetchArray()) { ?>
+    <tr>
+        <td><?= $row['id'] ?></td>
+        <td><?= $row['method'] ?></td>
+        <td><?= $row['uri']?></td>
+        <td><?= $row['query']?></td>
+        <td><?= $row['protocol']?></td>
+        <td><?= $row['time']?></td>
+        <td>
+            <table>
+<?php
+                $statement = $database->prepare(
+                    'SELECT * FROM request_header WHERE request_id=:id'
+                );
+                $statement->bindValue(':id', $row['id']);
+                $result = $statement->execute();
+                while ($row = $result->fetchArray()) {
+?>
+                <tr>
+                    <td>
+                        <pre><?= $row['name'] . ":" . $row['header'] ?></pre>
+                    </td>
+                </tr>
+<?php           } ?>
+            </table>
+        </td>
+    </tr>
+<?php       } ?>
+</table>
+<?php
+        } 
+    break;
+    }
+    default: {
+        header('HTTP/1.0 404 Not Found');
         break;
     }
 }
-
-require_once('Database.php');
